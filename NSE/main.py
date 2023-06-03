f6778e248a8a493fbb496d298fe31087000828b0
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2023-06-03 16:27:14 +0530

    sith commit

---
+++ NSE/main.py
@@ -0,0 +1,28 @@
+from nse import NSE
+
+class StockPrice:
+    def __init__(self, stock_name, stock_index='nse'):
+        self.stock_name = stock_name.strip()
+        self.stock_index = stock_index.strip().lower()
+        if self.stock_index == 'nse':
+            print(f'Searching for {self.stock_name} nse stock...')
+            self.stock = NSE(stock_name)
+            self.stock_name = self.stock.stock_name
+            print(f'Found stock based on provided name: {self.stock_name}\nStock instance created')
+        else:
+            print(f'{self.stock_index} stock scraping code under development.\nPlease wait for Update.')
+
+    
+    def get_latest_price(self):
+        return self.stock.get_latest_price()
+
+    def get_historical_data(self, from_date, to_date):
+        return self.stock.get_historical_data(from_date, to_date)
+    
+
+if __name__ == "__main__":
+    infosys_stock = StockPrice('infosys','nse')
+    latest_price_info = infosys_stock.get_latest_price()
+    historical_price_info = infosys_stock.get_historical_data('02-05-2023', '31-05-2023')
+    print(latest_price_info)
+    print(historical_price_info)
